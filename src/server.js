b675026d6fe4c97576afae61aa82f982bc923445
Serhii Tyron <mestn1y@hotmail.com>
2024-12-09 22:41:54 +0200

    add starts files

---
+++ src/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+const PORT = 3000;
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+  });
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
