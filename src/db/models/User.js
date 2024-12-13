f6849c432527f5f5c5907c241f8efd40faad66f6
Serhii Tyron <mestn1y@hotmail.com>
2024-12-14 00:58:48 +0200

    change db

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -4,14 +4,9 @@ import { emailRegexp, typeList } from '../../constants/users.js';
 
 const userSchema = new Schema(
   {
-    // gender: {
-    //   type: String,
-    //   enum: typeList,
-    //   default: 'woman',
-    // },
-
     name: {
       type: String,
+      default: 'User',
     },
 
     email: {
@@ -26,6 +21,12 @@ const userSchema = new Schema(
       required: true,
     },
 
+    gender: {
+      type: String,
+      enum: typeList,
+      default: 'woman',
+    },
+
     avatarUrl: {
       type: String,
       // default: null,
@@ -34,6 +35,7 @@ const userSchema = new Schema(
 
     daylyNorm: {
       type: Number,
+      default: 1800,
     },
     accessToken: {
       type: String,
