9b3794d07d04f36c69131b942c2e84dcce357b29
Serhii Tyron <mestn1y@hotmail.com>
2024-12-15 17:45:26 +0200

    change swagger/doc

--- src/routers/water.js
+++ src/routers/water.js
@@ -1,22 +1,21 @@
-import { Router } from "express";
-import { validateBody } from "../middlewares/validateBody.js";
-import { authenticate } from "../middlewares/authenticate.js";
-import { setWaterRateController } from "../controllers/water.js";
+import { Router } from 'express';
+import { validateBody } from '../middlewares/validateBody.js';
+import { authenticate } from '../middlewares/authenticate.js';
+import { setWaterRateController } from '../controllers/water.js';
 import { ctrlWrapper } from './../utils/ctrlWrapper.js';
 import { setWaterDaylyNormShema } from './../validation/water.js';
-
 
 const router = Router();
 
 // use authenticate?
 router.use(authenticate);
 
-  // response controllers
+// response controllers
 
-  router.post(
-    '/setwaterrate',
-    validateBody(setWaterDaylyNormShema),
-    ctrlWrapper(setWaterRateController),
-  );
+router.post(
+  '/add-water',
+  validateBody(setWaterDaylyNormShema),
+  ctrlWrapper(setWaterRateController),
+);
 
 export default router;
